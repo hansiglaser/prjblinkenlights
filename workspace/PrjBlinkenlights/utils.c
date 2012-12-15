86ad6b8aacb5ad77df70d266753580cb23698e2f
Johann Glaser <Johann.Glaser@gmx.at>
2012-12-15 19:58:27 +0100

    added comment on function of Int2BCD

--- workspace/PrjBlinkenlights/utils.c
+++ workspace/PrjBlinkenlights/utils.c
@@ -16,6 +16,26 @@ typedef union {
   };
 } TInt2BCDHelper;
 
+/**
+ * Convert an integer to a BCD value
+ *
+ * Note: only numbers from 0 to 9999 (i.e. 4 digits) are supported by the
+ * output data type.
+ *
+ * The function uses the Double dabble algorithm (see
+ * http://en.wikipedia.org/wiki/Double_dabble ) to avoid divisions.
+ *
+ * This uses a "scratch pad", which is the concatenation of the resulting
+ * BCD value and the original integer value. The union TInt2BCDHelper is
+ * introduced above to access this as one common 32-bit value as well as
+ * individual Integer and BCD values.
+ *
+ * The algorithm works as follows: For each of the input bits, the whole
+ * scratch pad is left-shifted. Before each shift, each BCD digit with a
+ * value above 4 is incremented by 3. Therefore 5 (left-shifted equals 10)
+ * is increased to 8 (left-shifted equals 16), and thus leading to a proper
+ * carry to the next BCD digit.
+ */
 uint16_t Int2BCD(uint16_t i) {
   TInt2BCDHelper Scratch;
   int n;
