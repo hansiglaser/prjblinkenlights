738f89944a169a75c7ebe1869068702b6a69fc6b
Johann Glaser <Johann.Glaser@gmx.at>
2012-12-15 22:01:40 +0100

    implemented RGB2HSV() and HSV2RGB(), still have to be tested

--- workspace/PrjBlinkenlights/color.c
+++ workspace/PrjBlinkenlights/color.c
@@ -47,7 +47,31 @@ uint16_t Brightness2PWM(uint16_t Brightness) {
  *
  */
 void RGB2HSV(const TColor* RGB, TColor* HSV) {
+  int Min = RGB->RGB.R;
+  if (RGB->RGB.G < Min) Min = RGB->RGB.G;
+  if (RGB->RGB.B < Min) Min = RGB->RGB.B;
+  int Max = RGB->RGB.R;
+  if (RGB->RGB.G > Max) Max = RGB->RGB.G;
+  if (RGB->RGB.B > Max) Max = RGB->RGB.B;
+  float Scale = (65536.0/6.0) / ((Max-Min)*1.0);
 
+  if (Min == Max) {
+    HSV->HSV.H = 0;
+  } else if (Max == RGB->RGB.R) {
+    HSV->HSV.H = floor((RGB->RGB.G - RGB->RGB.B)*1.0 * Scale);
+  } else if (Max == RGB->RGB.G) {
+    HSV->HSV.H = floor((RGB->RGB.B - RGB->RGB.R)*1.0 * Scale) + (65536/3);
+  } else /*  Max == RGB->RGB.R */ {
+    HSV->HSV.H = floor((RGB->RGB.R - RGB->RGB.G)*1.0 * Scale) + (65536/3)*2;
+  }
+
+  if (Max == 0) {
+    HSV->HSV.S = 0;
+  } else {
+    HSV->HSV.S = floor(((Max-Min)*65536.0)/(Max*1.0));
+  }
+
+  HSV->HSV.V = Max;
 }
 
 /**
@@ -73,7 +97,29 @@ void RGB2HSV(const TColor* RGB, TColor* HSV) {
  *
  */
 void HSV2RGB(const TColor* HSV, TColor* RGB) {
+  float f = ((HSV->HSV.H*1.0) / (65536.0/6.0));
+  int hi = floor(f);
+  f = f - hi;
+  int32_t p = HSV->HSV.V * (65536 - HSV->HSV.S) >> 16;
+  int32_t q = HSV->HSV.V * (65536 - (int32_t)floor((HSV->HSV.S*1.0)*f)) >> 16;
+  int32_t t = HSV->HSV.V * (65536 - (int32_t)floor((HSV->HSV.S*1.0)*(1.0-f))) >> 16;
+  int16_t V = HSV->HSV.V;
 
+  switch (hi) {
+  case 0:
+  case 6:
+    RGB->RGB.R = V; RGB->RGB.G = t; RGB->RGB.B = p; break;
+  case 1:
+    RGB->RGB.R = q; RGB->RGB.G = V; RGB->RGB.B = p; break;
+  case 2:
+    RGB->RGB.R = p; RGB->RGB.G = V; RGB->RGB.B = t; break;
+  case 3:
+    RGB->RGB.R = p; RGB->RGB.G = q; RGB->RGB.B = V; break;
+  case 4:
+    RGB->RGB.R = t; RGB->RGB.G = p; RGB->RGB.B = V; break;
+  case 5:
+    RGB->RGB.R = V; RGB->RGB.G = p; RGB->RGB.B = q; break;
+  }
 }
 
 /**
